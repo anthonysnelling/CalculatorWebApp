8f51fc852a4d437014c743975cf785f2723f8e34
Anthony Snelling <anthony.snelling@live.com>
2021-02-28 21:04:06 -0500

    added todos

--- script.js
+++ script.js
@@ -1,24 +1,24 @@
-// TODO: addition function
+// TODO: Add functionality for typing of buttons
+
+//TODO: add ability to grab string from text field.
+
+
 function additionFunc(a, b) {
   return a + b;
 }
 
-// TODO: subtraction function
 function subtractionFunc(a, b) {
   return a - b;
 }
 
-// TODO: multiplication function
 function multiplicationFunc(a, b) {
   return a * b;
 }
 
-// TODO: division function
 function divisionFunc(a, b) {
   return a / b;
 }
 
-// TODO: operate function, takes an operator & 2 numbers then calls one of the above functions on the numbers.
 function operateFunc(a, o, b) {
   if (o == "+") {
     return additionFunc(a, b);
