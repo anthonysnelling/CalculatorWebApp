4991793735b30df60a8559520ddb0421da7321b6
Anthony Snelling <anthony.snelling@live.com>
2021-02-28 19:36:51 -0500

    added styles.css, and added operation functions to scripts.js added some buttons to the html

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
-// TODO: additon function
-
+// TODO: addition function
+function additionFunc(a, b) {
+  return a + b;
+}
 
 // TODO: subtraction function
-
+function subtractionFunc(a, b) {
+  return a - b;
+}
 
 // TODO: multiplication function
-
+function multiplicationFunc(a, b) {
+  return a * b;
+}
 
 // TODO: division function
-
+function divisionFunc(a, b) {
+  return a / b;
+}
 
 // TODO: operate function, takes an operator & 2 numbers then calls one of the above functions on the numbers.
+function operateFunc(a, o, b) {
+  if (o == "+") {
+    return additionFunc(a, b);
+  } else if (o == "-") {
+      return subtractionFunc(a,b);
+  } else if(o == "*"){
+      return multiplicationFunc(a,b);
+  } else if(o == "/"){
+      return divisionFunc(a,b);
+  }
+}
