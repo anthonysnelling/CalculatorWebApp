42272fce52142cd20f6a7ed9aab941f51e901a9a
Anthony Snelling <anthony.snelling@live.com>
2021-03-03 21:00:35 -0500

    updated script.js to update the display, adjusted button layout on css. added button functionality in html to connect to script.js

--- script.js
+++ script.js
@@ -1,7 +1,46 @@
-// TODO: Add functionality for typing of buttons
+// TODO: Find a way to take the value from numberButtonsPressed and find the result of the string
+// also find a way to connect the keyboary keys to the buttons on screen. 
 
-//TODO: add ability to grab string from text field.
+function numberButtonsPressed(buttons) {
+  console.log(buttons);
+  let display = document.getElementById("calculatorDisplay");
+  // TODO: Use this to store the date VVVV
+  let currentOperation = "";
+  if (buttons == "oneButton") {
+    display.value += 1;
+  } else if (buttons == "twoButton") {
+    display.value += 2;
+  } else if (buttons == "threeButton") {
+    display.value += 3;
+  } else if (buttons == "fourButton") {
+    display.value += 4;
+  } else if (buttons == "fiveButton") {
+    display.value += 5;
+  } else if (buttons == "sixButton") {
+    display.value += 6;
+  } else if (buttons == "sevenButton") {
+    display.value += 7;
+  } else if (buttons == "eightButton") {
+    display.value += 8;
+  } else if (buttons == "nineButton") {
+    display.value += 9;
+  } else if (buttons == "zeroButton") {
+    display.value += 0;
+  } else if (buttons == "clearButton") {
+    display.value = "";
+  } else if (buttons == "plusButton" && display.value != "") {
+    display.value += "+";
+  } else if (buttons == "minusButton" && display.value != "") {
+    display.value += "-";
+  } else if (buttons == "divideButton" && display.value != "") {
+    display.value += "/";
+  } else if (buttons == "multiplyButton" && display.value != "") {
+    display.value += "*";
+  }
+}
 
+// clear the display
+function clear() {}
 
 function additionFunc(a, b) {
   return a + b;
@@ -23,10 +62,10 @@ function operateFunc(a, o, b) {
   if (o == "+") {
     return additionFunc(a, b);
   } else if (o == "-") {
-      return subtractionFunc(a,b);
-  } else if(o == "*"){
-      return multiplicationFunc(a,b);
-  } else if(o == "/"){
-      return divisionFunc(a,b);
+    return subtractionFunc(a, b);
+  } else if (o == "*") {
+    return multiplicationFunc(a, b);
+  } else if (o == "/") {
+    return divisionFunc(a, b);
   }
 }
