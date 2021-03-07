aeb9025a3851c62dc1bd6f5caac170c3cdabee25
Anthony Snelling <anthony.snelling@live.com>
2021-03-06 21:43:17 -0500

    Refactored code for readablity and simplicity, added keyboard press functionality

--- script.js
+++ script.js
@@ -7,10 +7,10 @@ let secondVar = "";
 let thirdVarInput = "";
 let result = "";
 let display = document.getElementById("calculatorDisplay");
+document.addEventListener("keypress", (e) => numberButtonsPressed(e.key));
 
 function numberButtonsPressed(buttons) {
   console.log(buttons);
-  // TODO: Use this to store the date VVVV
 
   if (currentOperation == "clearIT") {
     display.value = "";
@@ -25,60 +25,59 @@ function numberButtonsPressed(buttons) {
     thirdVarInput == true;
   }
 
-  if (buttons == "oneButton") {
+  if (buttons == "oneButton" || buttons == "1") {
     display.value += 1;
-  } else if (buttons == "twoButton") {
+  } else if (buttons == "twoButton" || buttons == "2") {
     display.value += 2;
-  } else if (buttons == "threeButton") {
+  } else if (buttons == "threeButton" || buttons == "3") {
     display.value += 3;
-  } else if (buttons == "fourButton") {
+  } else if (buttons == "fourButton" || buttons == "4") {
     display.value += 4;
-  } else if (buttons == "fiveButton") {
+  } else if (buttons == "fiveButton" || buttons == "5") {
     display.value += 5;
-  } else if (buttons == "sixButton") {
+  } else if (buttons == "sixButton" || buttons == "6") {
     display.value += 6;
-  } else if (buttons == "sevenButton") {
+  } else if (buttons == "sevenButton" || buttons == "7") {
     display.value += 7;
-  } else if (buttons == "eightButton") {
+  } else if (buttons == "eightButton" || buttons == "8") {
     display.value += 8;
-  } else if (buttons == "nineButton") {
+  } else if (buttons == "nineButton" || buttons == "9") {
     display.value += 9;
-  } else if (buttons == "zeroButton") {
+  } else if (buttons == "zeroButton" || buttons == "0") {
     display.value += 0;
-  } else if (buttons == "clearButton") {
+  } else if (buttons == "clearButton" || buttons == "c") {
     display.value = "";
-  } else if (buttons == "plusButton") {
-    if (firstVar == "" && secondVar == "" && display.value != "") {
-      firstVar = parseFloat(display.value);
-    }
-    currentOperation = "+";
-    if (thirdVarInput == "") {
-      display.value = "";
-    }
+    firstVar = "";
+    secondVar = "";
+    thirdVarInput = "";
+  } else if (buttons == "plusButton" || buttons == "+") {
+    additionFunc(firstVar, secondVar);
   } else if (buttons == "minusButton" && display.value != "") {
-    firstVar = display.value;
-    currentOperation = "-";
-    display.value += "";
-  } else if (buttons == "divideButton" && display.value != "") {
-    display.value += "/";
-  } else if (buttons == "multiplyButton" && display.value != "") {
-    display.value += "*";
-  } else if (buttons == "equalsButton") {
-    secondVar = parseFloat(display.value);
-    console.log(firstVar);
-    console.log(currentOperation);
-    console.log(secondVar);
-    result = operateFunc(firstVar, currentOperation, secondVar);
-    display.value = result;
-
-    currentOperation = "clearIT";
+    subtractionFunc(firstVar, secondVar);
+  } else if (buttons == "divideButton" && display.value != "/") {
+    divisionFunc(firstVar, secondVar);
+  } else if (buttons == "multiplyButton" && display.value != "*") {
+    multiplicationFunc(firstVar, secondVar);
+  } else if (buttons == "equalsButton" || "Enter") {
+    operateFunc(currentOperation);
   }
-
 }
 
-
 function additionFunc(a, b) {
-  return a + b;
+  currentOperation = "+";
+  if (a == "" && b == "") {
+    firstVar = display.value;
+    display.value = "";
+  } else if (a != "" && b == "") {
+    secondVar = display.value;
+    result = parseFloat(firstVar) + parseFloat(secondVar);
+    firstVar = result;
+    secondVar = "";
+    display.value = result;
+  } else if (a != "" && b != "") {
+    result = a + b;
+    display.value = result;
+  }
 }
 
 function subtractionFunc(a, b) {
@@ -93,7 +92,7 @@ function divisionFunc(a, b) {
   return a / b;
 }
 
-function operateFunc(a, o, b) {
+function operateFunc(o) {
   if (o == "+") {
     return additionFunc(a, b);
   } else if (o == "-") {
